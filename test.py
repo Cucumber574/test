2643f02736455361e000fce5827fe251b97e7f64
Cucumber574 <145482899+Cucumber574@users.noreply.github.com>
2023-09-19 19:21:16 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+temperature=-41
+
+if  temperature < -40:
+    print("Захвати шапку")
+
+elif temperature < 0:
+      print("Ничего не нужно")
+elif:temperature < 20
+      print("Захвати крем") 
+else 
+    print("Возьми шубу")      
